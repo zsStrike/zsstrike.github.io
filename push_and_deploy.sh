893a63fafdb376bdea386bffb466327e687aff36
zsStrike <2439758713@qq.com>
2022-11-29 21:57:29 +0800

    add new post

--- push_and_deploy.sh
+++ push_and_deploy.sh
@@ -3,7 +3,7 @@
 # push code to hexo-project
 git add .
 git commit -m "auto push via push_and_deploy.sh"
-git push origin hexo-project
+git push -f origin hexo-project
 # deploy origin master
 hexo clean
 hexo g -d
